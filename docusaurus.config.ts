8c4539c8f5e824b7ddf1c928f4594ec07860f6df
Imani brown <76929595+itszavier@users.noreply.github.com>
2025-03-14 22:42:44 +0000

    small changes added the correct information

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,8 +42,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/DiscoraFramework/discora-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
@@ -53,8 +52,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/DiscoraFramework/discora-docs/tree/main/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
@@ -85,7 +83,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "right" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/DiscoraFramework/",
           label: "GitHub",
           position: "right",
         },
@@ -105,28 +103,12 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
+              label: "documention",
+              to: "/",
             },
           ],
         },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "X",
-              href: "https://x.com/docusaurus",
-            },
-          ],
-        },
+
         {
           title: "More",
           items: [
@@ -136,12 +118,13 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+
+              href: "https://github.com/DiscoraFramework",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} DiscoraFramework`,
     },
     prism: {
       theme: prismThemes.github,
